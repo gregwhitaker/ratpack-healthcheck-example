191554d626ef829e20f21d04aeeac12406494d40
gregwhitaker <gwhitake@gmail.com>
2018-03-18 18:30:50 -0700

    Doing work

--- src/main/java/ratpack/healthcheck/example/api/ApiModule.java
+++ src/main/java/ratpack/healthcheck/example/api/ApiModule.java
@@ -1,6 +1,7 @@
 package ratpack.healthcheck.example.api;
 
 import com.google.inject.AbstractModule;
+import ratpack.health.HealthCheckHandler;
 import ratpack.healthcheck.example.api.bar.BarHandler;
 import ratpack.healthcheck.example.api.foo.FooHandler;
 
@@ -10,6 +11,7 @@ public class ApiModule extends AbstractModule {
     protected void configure() {
         bind(ApiEndpoints.class);
 
+        bind(HealthCheckHandler.class);
         bind(FooHandler.class);
         bind(BarHandler.class);
     }
